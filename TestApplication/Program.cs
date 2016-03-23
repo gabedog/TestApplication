6e2c7e21d4bfd52820d0efeed8d2480a0644ec52
Gabedog <satch1917@live.com>
2016-03-23 15:44:28 -0500

    added new comments for sync

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -21,6 +21,17 @@ namespace TestApplication
 
             //Updating Feature1
 
+            //adding a comment... this will be to test just sync..
+            //there were changes on the server.
+            //these comments are new.
+            //will it automatically do the pull, and push??? we'll see...
+            // 
+            // do sync
+            // check github server.... does it have these comments.
+
+            //syncing... just synched the incoming and outgoing commits.
+            //note: sync can only be done when there are no uncommitted changes.
+            //      committed but not pushed is ok...
 
         }
     }
